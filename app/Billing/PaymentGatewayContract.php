5ce984dda054a46cfca28393e88d8a217b74c033
Mr-Hera <randellhera@gmail.com>
2023-10-02 16:08:34 +0300

    refactor: added descriptive comments for code base

--- app/Billing/PaymentGatewayContract.php
+++ app/Billing/PaymentGatewayContract.php
@@ -5,6 +5,9 @@ namespace App\Billing;
 
 interface PaymentGatewayContract
 {
+    // set discount if any
     public function setDiscount($amount);
+
+    // charge the bank
     public function charge($amount);
 }
